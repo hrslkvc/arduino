02644461b9c59e786d8039b130e0acc9dcc5518a
haris <hareluuu@gmail.com>
2019-01-13 14:46:18 +0100

    Update comments to clarify every step of the program

--- FadePotentiometer/FadePotentiometer.ino
+++ FadePotentiometer/FadePotentiometer.ino
@@ -1,36 +1,29 @@
 /*
-  Fade
-
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fade
+  Fade two LEDs with a potentiometer
 */
 
 int led1 = 9;           // the PWM pin the LED is attached to
 int led2 = 11;
-int brightness1 = 0;    // how bright the LED is
-int brightness2 = 255 - brightness1;    // how bright the LED is
+int brightness1 = 0;    // how bright LED1 is
+int brightness2 = 255 - brightness1;    // how bright LED2 is
 
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // declare pin 9 to be an output:
+  // declare pins 9 and 11 to be LED outputs:
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
+  // serial input for the potentiometer
   Serial.begin(9600);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+  // read analog input from the potentiometer, divide it by 4 since it's range (0-1023)is bigger than the brightness output(0-255) value
   int brightness1 = analogRead(A0) / 4;
+  // subtract LED1 brightness from 255 to get the oposite value and use it for LED2
   int brightness2 = 255 - brightness1; 
-  // set the brightness of pin 9:
+  // update LED brightness according to the calculated analog input
   analogWrite(led1, brightness1);
-  analogWrite(led2, brightness2);}
+  analogWrite(led2, brightness2);
+}
